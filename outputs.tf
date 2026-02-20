ded7b7b714c0d6aa5c882d4ed96c775c1a4a4e6c
Ranjithkumar <ranjith.kumar@aquera.com>
2026-02-20 15:59:05 +0530

    fixed

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "api_base_url" {
   description = "Base URL for API"
   value       = aws_apigatewayv2_api.main.api_endpoint
 }
+
+output "api_id" {
+  description = "API Gateway ID"
+  value       = aws_apigatewayv2_api.main.id
+}
